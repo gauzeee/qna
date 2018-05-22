e518bd4795f1b278f458db3e6f9b55e64c9b813e
gauzeee <gauzeee@gmail.com>
2018-05-22 13:31:07 +0300

    Add Answer model and tests

--- db/migrate/20180522101159_create_answers.rb
+++ db/migrate/20180522101159_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[5.2]
   def change
     create_table :answers do |t|
-      t.string :title
       t.text :body
+      t.references :question, foreing_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,11 @@ ActiveRecord::Schema.define(version: 2018_05_22_101159) do
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.string "title"
     t.text "body"
+    t.bigint "question_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
