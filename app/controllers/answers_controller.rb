b6ae94bf29ec3a3782a41ad3a2f5ab5c43742f73
gauzeee <gauzeee@gmail.com>
2018-05-23 12:50:54 +0300

    Add specs, views and actions to AnswersController

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,36 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i(new create)
+  before_action :find_answer, only: :show
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  def show
+  end
+
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @answer.save
+      redirect_to answer_path(@answer)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
+
