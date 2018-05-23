8b268b93b597b4caa4feab241bb61f8f78a0ad3f
gauzeee <gauzeee@gmail.com>
2018-05-23 11:54:58 +0300

    Add Answers Controller

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  describe 'GET #show' do
+    it 'populates an array of answers for question' do
+    end
+
+    it 'render'
+
+  end
+
+end
