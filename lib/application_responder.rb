b178ba8de6a0f72d0edb30c7a385b8a376dd9263
gauzeee <gauzeee@gmail.com>
2018-07-10 14:40:50 +0300

    Fixes

--- lib/application_responder.rb
+++ lib/application_responder.rb
@@ -5,4 +5,14 @@ class ApplicationResponder < ActionController::Responder
   # Redirects resources to the collection path (index action) instead
   # of the resource path (show action) for POST/PUT/DELETE requests.
   # include Responders::CollectionResponder
+  if env.respond_to?(:register_transformer)
+    env.register_mime_type 'text/css', extensions: ['.css'], charset: :css
+    env.register_preprocessor 'text/css', MySprocketsExtension
+  end
+
+  if env.respond_to?(:register_engine)
+    args = ['.css', MySprocketsExtension]
+    args << { mime_type: 'text/css', silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+    env.register_engine(*args)
+  end
 end
