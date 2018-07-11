dbd39d0518e8ab6612ce38f725b10b4effc3f4ed
gauzeee <gauzeee@gmail.com>
2018-07-11 13:21:21 +0300

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: 'admin@qna.com', encrypted_password: 'password', admin: true, created_at: Time.now, confirmed_at: Time.now)
+User.create(email: 'admin@qna.com', password: 'password', admin: true, confirmed_at: Time.now)
 
-questions = Question.create([
-  { title: 'The first question'}, { body: 'I want to ask you some...'}, { user_id: User.first.id },
-  { title: 'The second question'}, { body: 'I want to ask you some more...'}, { user_id: User.first.id },
-  { title: 'The third question'}, { body: 'Let`s fix all bugs man!'}, { user_id: User.first.id }])
+Question.create(title: 'The first question', body: 'I want to ask you some...',  user_id: User.first.id)
+Question.create(title: 'The second question', body: 'I want to ask you some more...', user_id: User.first.id)
 
-answers = Answer.create([
-  { body: 'Test answer'}, { question_id: questions[0].id }, { user_id: User.first.id },
-  { body: 'Test answer'}, { question_id: questions[1].id }, { user_id: User.first.id },
-  { body: 'Test answer'}, { question_id: questions[2].id }, { user_id: User.first.id }])
+Answer.create(body: 'Test 1 answer', question_id: Question.first.id, user_id: User.first.id)
+Answer.create(body: 'Test 2 answer', question_id: Question.last.id, user_id: User.first.id)
 
-comments = Comment.create([
-{ body: 'New comment' }, { commentable_type: 'Question' }, { commentable_id: questions[0].id }, { user_id: User.first.id },
-{ body: 'New comment' }, { commentable_type: 'Answer' }, { commentable_id: answers[0].id }, { user_id: User.first.id },
-{ body: 'New comment' }, { commentable_type: 'Question' }, { commentable_id: questions[2].id }, { user_id: User.first.id }])
+Comment.create(body: 'New 1 comment', commentable_type: 'Question', commentable_id: Question.first.id, user_id: User.first.id)
+Comment.create(body: 'New 2 comment', commentable_type: 'Question', commentable_id: Question.last.id, user_id: User.first.id)
+Comment.create(body: 'New 3 comment', commentable_type: 'Answer', commentable_id: Answer.first.id, user_id: User.first.id)
+Comment.create(body: 'New 4 comment', commentable_type: 'Answer', commentable_id: Answer.last.id, user_id: User.first.id)
+
